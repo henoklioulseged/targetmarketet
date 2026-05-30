2fb679f50b22141c5c8e49af511c4c506f01c9b5
henoklioulseged <henoklioulseged118@gmail.com>
2026-05-30 23:14:10 +0300

    Add Firebase Firestore integration for post submission

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 alert("APP STARTED");
 
+import {
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+alert("IMPORT OK");
+
 const buttons = document.querySelectorAll("button");
-alert("Buttons Found: " + buttons.length);
+const postBtn = buttons[1];
+
+postBtn.addEventListener("click", async () => {
+  alert("BUTTON CLICKED");
+
+  try {
+    await addDoc(collection(window.db, "posts"), {
+      name: "Test"
+    });
+
+    alert("SUCCESS");
+  } catch (error) {
+    alert("ERROR: " + error.message);
+    console.error(error);
+  }
+});
