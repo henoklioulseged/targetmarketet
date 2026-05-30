2c90688541d827feca00c6e5ae0c6b04ffe604c3
henoklioulseged <henoklioulseged118@gmail.com>
2026-05-30 22:47:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-console.log("Target Market ET Started");
+import {
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  alert("Target Market ET Ready");
+const postBtn = document.querySelector("button");
+
+postBtn.addEventListener("click", async () => {
+  try {
+    await addDoc(collection(window.db, "posts"), {
+      seller: "Test User",
+      phone: "0948779378",
+      product: "Test Product",
+      price: 100,
+      createdAt: new Date()
+    });
+
+    alert("ዕቃው በተሳካ ሁኔታ ተመዝግቧል!");
+  } catch (error) {
+    console.error(error);
+    alert("ስህተት ተፈጥሯል");
+  }
 });
