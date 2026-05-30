2e3e82a99633e5f511fe3dddf2d204944d9e194f
henoklioulseged <henoklioulseged118@gmail.com>
2026-05-30 16:52:45 +0300

    Add Firebase configuration to firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,11 @@
-console.log("Firebase connected");
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyY0EgAxSXTw1EMR935Ub5y5UPHK4Nd9E",
+  authDomain: "target-market-et.firebaseapp.com",
+  projectId: "target-market-et",
+  storageBucket: "target-market-et.firebasestorage.app",
+  messagingSenderId: "731946391519",
+  appId: "1:731946391519:web:a817fc424882f332c6d66a"
+};
+
+console.log("Firebase Ready");
