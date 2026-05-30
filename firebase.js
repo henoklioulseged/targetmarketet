2b02ff6003b2af295162d4f432a64934163ae02f
henoklioulseged <henoklioulseged118@gmail.com>
2026-05-30 22:28:53 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,8 @@
-// Firebase configuration
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyCyY0EgAxSXTw1EMR935Ub5y5UPHK4Nd9E",
   authDomain: "target-market-et.firebaseapp.com",
@@ -8,4 +12,10 @@ const firebaseConfig = {
   appId: "1:731946391519:web:a817fc424882f332c6d66a"
 };
 
-console.log("Firebase Ready");
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.db = db;
+
+console.log("Firebase Connected Successfully");
