36941ddbe82c3f41cdc241eb56fcae42beec59e6
henoklioulseged <henoklioulseged118@gmail.com>
2026-05-30 23:12:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-
-import {
-  collection,
-  addDoc
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+alert("APP STARTED");
 
 const buttons = document.querySelectorAll("button");
-const postBtn = buttons[1];
-
-postBtn.addEventListener("click", async () => {
-  try {
-    await addDoc(collection(window.db, "posts"), {
-      seller: "Test User",
-      phone: "0948779378",
-      product: "Test Product",
-      price: 100,
-      createdAt: new Date()
+alert("Buttons Found: " + buttons.length);
